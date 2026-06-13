6fdfae018858fd0cee933d4a8698bbfa3f9a37f3
RomanMateos <rom.suarez@duocuc.cl>
2026-06-13 17:25:39 -0400

    carpetas completas con su respectivo Spring boot y paquetes realizados.

---
+++ ms-autor/autor/src/test/java/com/fullstackev2/autor/AutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackev2.autor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ms-prestamo/prestamo/src/test/java/com/fullstackev2/prestamo/PrestamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackev2.prestamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
